834511cbb56435c71e94954fb28a3e77ab6fb892
jinu729 <timytunner@gmail.com>
2024-07-23 15:04:22 +0900

    초기 셋팅

--- omg_back/app.js
+++ omg_back/app.js
@@ -11,20 +11,19 @@ app.use(cors({      // cors 설정을 해줘야 front 서버와 통신 가능
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const authRouter = require('./routes/auth.js');
-// const userRouter = require('./routes/user.js');
-// const trainerRouter = require('./routes/trainer.js');
-// const adminRouter = require('./routes/admin.js');
+const authRouter = require('./routes/auth.js');
+const userRouter = require('./routes/user.js');
+const movieRouter = require('./routes/movie.js');
+const adminRouter = require('./routes/admin.js');
+const storeRouter = require('./routes/store.js');
 
-// app.use('/auth', authRouter);
-// app.use('/user', userRouter);
-// app.use('/trainer', trainerRouter);
-// app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+app.use('/user', userRouter);
+app.use('/movie', movieRouter);
+app.use('/admin', adminRouter);
+app.use('/store', storeRouter);
 
 // 테스트 라우트 추가
-app.get('/test', (req, res) => {
-    res.status(200).send('Server is connected and running!');
-});
 
 
 app.listen(3000, function() {
