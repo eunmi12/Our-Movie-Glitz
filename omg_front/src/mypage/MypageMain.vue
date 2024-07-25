854b1b76dcee95bc24b0a68c987879b2e4880f3c
kimhoechang <ghlckd1999@naver.com>
2024-07-25 17:32:23 +0900

    역대급 디자인

--- omg_front/src/mypage/MypageMain.vue
+++ omg_front/src/mypage/MypageMain.vue
@@ -1,17 +1,72 @@
 <template>
     <div class="container">
-        <div class="profile">
-            
+      <MypageTop />
+      <div class="mypagemain">
+        <MypageSideBar />
+        <div class="mypagebox">
+          <div class="rev_box">
+            <p class="text1">MY 예매내역</p>
+          </div>
+          <div class="qna_box">
+            <p class="text1">MY 문희내역</p>
+          </div>
+          <div class="rev_box">
+            <p class="text1">MY 리뷰 내역</p>
+          </div>
         </div>
+      </div>
     </div>    
-</template>
-
-<script>
-export default {
-    
-}
-</script>
-
-<style scoped>
-
-</style>
+  </template>
+  
+  <script>
+  import MypageSideBar from '../layouts/MypageSideBar.vue';
+  import MypageTop from '../layouts/MypageTop.vue';
+  
+  export default {
+    components: {
+      MypageTop,
+      MypageSideBar,
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 15px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+  
+  .mypagemain {
+    display: flex;
+    margin-top: 20px;
+  }
+  
+  .mypagebox {
+    width: 100%;
+    margin-left: 100px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+  }
+  
+  .rev_box, .qna_box {
+    border: 1px solid #f0eeda;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: #f9f9f9;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+  }
+  
+  .text1 {
+    text-align: left;
+    font-weight: bold;
+    border-bottom: 1px solid rgb(179, 177, 177);
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+  }
+  </style>
+  
---
+++ omg_front/src/mypage/UserOut.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container">
+        <MypageTop />
+        <div class="mypagemain">
+            <MypageSideBar />
+            <div class="mypagebox">
+                <div class="rev_box">
+                    <div class="text1">비밀번호 확인</div>
+                    <div class="text2">고객님의 개인정보 보호를 위한 절차이오니, OMG 로그인 시 사용하는 비밀번호를 입력해 주세요.</div>
+                    <div class="id_check">
+                        <div class="id"> ghlckd1999</div>
+                        <div class="pw"> zzz</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+import MypageSideBar from '../layouts/MypageSideBar.vue';
+import MypageTop from '../layouts/MypageTop.vue';
+
+export default {
+    components: {
+        MypageTop,
+        MypageSideBar,
+    }, 
+
+}
+</script>
+
+<style scoped>
+.container {
+    width: 80%;
+
+}
+.mypagemain {
+    display:flex;
+}
+.mypagebox{
+    width: 100%;
+    margin-left: 100px;
+    margin-top: 20px;
+}
+.rev_box {
+    border: 1px solid black;
+    margin-bottom: 30px;
+    padding-left: 30px;
+}
+.qna_box {
+    border: 1px solid black;
+    margin-bottom: 30px;
+}
+.rev_box {
+    border: 1px solid black;
+}
+.text1 {
+    text-align: left;
+    margin: 20px auto;
+    font-weight: bold;
+    border-bottom: 1px solid rgb(179, 177, 177);
+}
+.text2 {
+    text-align: left;
+    margin: auto;
+    font-weight: bold;
+    border-bottom: 1px solid rgb(179, 177, 177);
+}
+</style>
