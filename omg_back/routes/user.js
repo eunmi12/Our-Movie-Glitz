356fa0f0f4545e87351bf14998ab39814329bef3
youngjl1105 <wodud5387@Gmail.com>
2024-08-06 17:23:47 +0900

    080052

--- omg_back/routes/user.js
+++ omg_back/routes/user.js
@@ -34,6 +34,7 @@ router.post('/eventlist', (req, res) => {
       }
     });
 });
+
 router.get('/maineventlist', (req, res) => {
     const sql = `
         select event_no, event_img1, event_title,date_format(event_startdate, '%y-%m-%d') as event_startdate, date_format(event_enddate, '%y-%m-%d') as event_enddate from event order by event_no desc;
@@ -46,6 +47,7 @@ router.get('/maineventlist', (req, res) => {
       }
     });
 });
+
 router.post('/deleteevent', (req, res) => {
     db.query(`DELETE FROM event WHERE event_no = ?`,[req.body.event_no] , (err, results) => {
         if(err) {
