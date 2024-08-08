af6efe7693a40a42b1e24f175fabe4cc11cca3a8
eunmi <mcspygirl2@naver.com>
2024-08-08 17:25:21 +0900

    eu/이벤트페이지리스트/프로시저트리거박살

--- omg_front/src/views/EventList.vue
+++ omg_front/src/views/EventList.vue
@@ -4,28 +4,68 @@
             EVENT
         </div>
         <div class="eventcontent">
-            <div class="eventimg">
-                이미지<img style="width:256px; height: 170px;" src="../images/mainpage/events/1-0.png">
-                <div>
-                    내용
+            <div class="content_wrap">
+                <div class="content_detail">
+                    <div class="sort">
+                        <select id="sort" @change="sortoption">
+                            <option id="listing" value="eventlisting">진행 중인 이벤트</option>
+                            <option id="listend" value="eventlistend">종료된 이벤트</option>
+                        </select>
+                    </div>
+                    <div class="ul_content">
+                        <ul class="event_content">
+                            <li v-for="(event, i) in event" :key="i">
+                                <a :href="'http://localhost:8081/EventDetail/' + event.event_no" @click="upeventcnt(event.event_no)">
+                                    <div class="event_img">
+                                        <img src="../images/mainpage/events/이벤트3.jpg">
+                                    </div>
+                                    <div class="event_text">
+                                        <p class="text1">
+                                            {{event.event_title}}
+                                        </p>
+                                        <p class="text2">
+                                            {{event.event_startdate}} ~ {{event.event_enddate}}
+                                        </p>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="eventimg">
-                <img style="width:256px; height: 170px;" src="../images/mainpage/events/이벤트3.jpg">
-                <div>
-                    내용
-                </div>
-            </div>
-            
-
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
-}
+    data() {
+        return {
+            event: [],
+        };
+    },
+    created() {
+        this.getEventList('eventlisting');
+    },
+    methods: {
+        async getEventList(endpoint) {
+            try {
+                const response = await axios.post(`http://localhost:3000/user/${endpoint}`);
+                const data = response.data;
+                this.event = data;
+                console.log('eventlist', this.event);
+            } catch (error) {
+                console.error('이벤트 리스트 불러오는 도중 에러 발생', error);
+            }
+        },
+        sortoption(event) {
+            const selectedValue = event.target.value;
+            this.getEventList(selectedValue);
+        },
+    },
+};
 </script>
 
 <style scoped>
@@ -35,14 +75,86 @@ export default {
 }
 .eventtitle{
     font-size:36px;
-    margin-left:3%;
+    /* margin-left:3%; */
     margin-top:100px;
     padding-bottom: 45px;
     border-bottom: 4px solid #ccc;
 }
 .eventcontent{
     width:100%;
+}
+.content_detail{
+    display: flex;
+    flex-direction: column;
+}
+.sort{
+    padding-top:30px;
+    text-align: right;
+}
+#sort{
+    width: 150px;
+    height: 35px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    
+}
+#listing #listend{
+    width:30px;
+    height: 40px;
+    border: 1px solid #000;
+    
+}
+.event_content{
+    margin-top:20px;
     display: flex;
     flex-wrap: wrap;
+    /* justify-content: space-between; */
+    justify-content: flex-start;
+    padding-left:0;
+}
+.event_content li:nth-child(5n+1){
+    margin: 10px 0 13px 0;
+
+}
+.event_img img{
+    width:256px;
+    height:170px;
+}
+
+.event_content li a{
+    display: block;
+    width:100%;
+    height:100%;
+}
+.event_content li{
+    margin: 10px 0 13px 60px;
+    background: #fff;
+}
+.event_text{
+    min-height: 90px;
+    padding-bottom: 17px;
+    border: 1px solid #dddfe2;
+    border-top: 0;
+}
+.text1{
+    padding: 18px 11px 0 11px;
+    height: 38px;
+    vertical-align: top;
+    font-size: 16px;
+    text-decoration: none;
+    color: #000;
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+    white-space: normal;
+    letter-spacing: -1px;
+}
+.text2{
+    color: #6a6f77;
+    font-size: 13px;
+    padding: 17px 11px 0 11px;
+    letter-spacing: -0.5px;
 }
 </style>
